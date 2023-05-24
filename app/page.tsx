459d4d30e2ed523228208bd1f4f34dc8fd31ea17
Gaspi <gasparelpi@gmail.com>
2023-05-24 04:44:54 -0300

    fixing build error 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-export const dynamic = 'auto'
 import { getCurrentUser } from "./actions/getCurrentUser";
 import getListings, { IListing } from "./actions/getListings";
 import ClientOnly from "./components/ClientOnly";
@@ -11,23 +10,39 @@ interface HomeProps {
 }
 
 const Home = async ({ searchParams }: HomeProps) => {
-  const listings = await getListings(searchParams)
-  const currentUser = await getCurrentUser()
+  const listings = await getListings(searchParams);
+  const currentUser = await getCurrentUser();
 
   if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
 
   return (
     <ClientOnly>
       <Container>
-        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
-          {listings.map((listing) => (
-            <ListingCard currentUser={currentUser} key={listing.id} data={listing} />
+        <div 
+          className="
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
+        >
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
           ))}
         </div>
       </Container>
@@ -35,4 +50,4 @@ const Home = async ({ searchParams }: HomeProps) => {
   )
 }
 
-export default Home
+export default Home;
